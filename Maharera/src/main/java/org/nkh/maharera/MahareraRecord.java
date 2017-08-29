8d5bb6dddfd9b1085a0ec6cf9f468301a210d6aa
Niranjan Khare <nkh@ums.no>
2017-08-29 22:56:32 +0530

    Project Info parsed

--- Maharera/src/main/java/org/nkh/maharera/MahareraRecord.java
+++ Maharera/src/main/java/org/nkh/maharera/MahareraRecord.java
@@ -13,23 +13,22 @@ public class MahareraRecord {
 	private URL remoteReference = null;
 	private Document record = null;
 	private String inferredType = "Individual";
-	private String typeInfo= "";
-	private String ownerName= "";
-	private String projectName= "";
+	private String typeInfo = "";
+	private String ownerName = "";
+	private String projectName = "";
 	private String pinCode = "";
-	private String division= "";
-	private String district= "";
-	private String taluka= "";
-	private String village= "";
-	private String projectArea= "";
-	private String projectType= "";
-		
+	private String division = "";
+	private String district = "";
+	private String taluka = "";
+	private String village = "";
+	private String projectArea = "";
+	private String projectType = "";
+
 	public List<BuildingRecord> Buildings = new ArrayList<BuildingRecord>();
 	private Element rawType = null;
 	private Element divProject = null;
-	
-	
-	public MahareraRecord (Document source ){
+
+	public MahareraRecord(Document source) {
 		this.record = source;
 		setInferredType();
 		setTypeInfo();
@@ -39,63 +38,82 @@ public class MahareraRecord {
 		setDivisionDistrict();
 		setTalukaVillage();
 		setProjectType(record);
-		this.Buildings = getBuildings (record);
-	}
-	
-	
-	 private void setInferredType() {
-		rawType  = record.getElementById("fldind");
-		if (rawType == null )
-			rawType = record.getElementById("fldFirm");		
+		this.Buildings = getBuildings(record);
 	}
 
-	 private void setTypeInfo() {
-		Element typeInfo = record.getElementById("divInfoType");
-		Element y = typeInfo.getElementsByAttributeValue("for", "PersonalInfoModel_InfoTypeValue").get(0);
-//		return y.parent().siblingElements().text();
+	private void setInferredType() {
+		rawType = record.getElementById("fldind");
+		if (rawType == null)
+			rawType = record.getElementById("fldFirm");
 	}
 
-	 private void setOwner() {
-		 String info = rawType.getElementsByClass("x_panel").get(0).getElementsByClass("x_content").get(0).getElementsByClass("row").get(0).text();
-		ownerName= info;
-
-	}
-	
-	 private void setProjectName() {
-		divProject   = record.getElementById("DivProject");
-		String info = divProject.getElementsByClass("x_panel").get(0).getElementsByClass("x_content").get(0).getElementsByClass("row").get(0).text();
-		
-	}
-	 
-
-	 private void setPinCodeAreaSqM() {
-		Element rootElement = divProject.getElementsByClass("x_panel").get(0).getElementsByClass("x_content").get(1).getElementsByClass("row").get(7);
-		String pinCode = rootElement.text();
-		
+	private void setTypeInfo() {
+		Element info = record.getElementById("divInfoType");
+		Element y = info.getElementsByAttributeValue("for", "PersonalInfoModel_InfoTypeValue").get(0);
+		this.typeInfo = y.parent().siblingElements().text();
 	}
 
-	 private void setDivisionDistrict() {
-		
+	private void setOwner() {
+		String info = rawType.getElementsByClass("x_panel").get(0).getElementsByClass("x_content").get(0)
+				.getElementsByClass("row").get(0).text();
+		this.ownerName = info;
+
 	}
 
-//	public static String getDistrict(Document doc) {
-//		return null;
-//	}
-
-	 private void setTalukaVillage() {
-		
+	private void setProjectName() {
+		this.divProject = record.getElementById("DivProject");
+		String info = divProject.getElementsByClass("x_panel").get(0).getElementsByClass("x_content").get(0)
+				.getElementsByClass("row").get(0).text();
+		this.projectName = info;
 	}
 
-//	public static String getVillage(Document doc) {
-//		return null;
-//	}
+	private void setPinCodeAreaSqM() {
+		Element rootElement = divProject.getElementsByClass("x_panel").get(0).getElementsByClass("x_content").get(1)
+				.getElementsByClass("row").get(7).getElementsByClass("form-group").get(0);
 
-//	 private void setProjectAreaSqM(Document doc) {
-//		return null;
-//	}
+		Element pin = rootElement.children().get(1);
+		this.pinCode = pin.text();
 
-	 private void setProjectType(Document doc) {
-		
+		Element area = rootElement.children().get(3);
+		this.projectArea = area.text();
+	}
+
+	private void setDivisionDistrict() {
+		Element rootElement = divProject.getElementsByClass("x_panel").get(0).getElementsByClass("x_content").get(1)
+				.getElementsByClass("row").get(5).getElementsByClass("form-group").get(0);
+
+		Element div = rootElement.children().get(1);
+		this.division = div.text();
+
+		Element dist = rootElement.children().get(3);
+		this.district = dist.text();
+	}
+
+	// public static String getDistrict(Document doc) {
+	// return null;
+	// }
+
+	private void setTalukaVillage() {
+		Element rootElement = divProject.getElementsByClass("x_panel").get(0).getElementsByClass("x_content").get(1)
+				.getElementsByClass("row").get(6).getElementsByClass("form-group").get(0);
+
+		Element tal = rootElement.children().get(1);
+		this.taluka = tal.text();
+
+		Element vil = rootElement.children().get(3);
+		this.village = vil.text();
+	}
+
+	// public static String getVillage(Document doc) {
+	// return null;
+	// }
+
+	// private void setProjectAreaSqM(Document doc) {
+	// return null;
+	// }
+
+	private void setProjectType(Document doc) {
+
 	}
 
 	public static String buildingNo() {
@@ -155,6 +173,6 @@ public class MahareraRecord {
 		return null;
 	}
 	/**
-	 *	Pin Code	Division	District	Taluka	Village	Area(In sqmts)	Project Type
+	 * Pin Code Division District Taluka Village Area(In sqmts) Project Type
 	 */
 }
